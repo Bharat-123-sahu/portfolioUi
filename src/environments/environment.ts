618f1803d61e14b3227c1d35f539808e3d6a5ac2
bharat sahu <bharatsahu0037@gmail.com>
2026-08-05 00:40:54 +0530

    true

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   apiUrl: 'https://portfolioapi-cavv.onrender.com',
   tokenKey: 'Bharat_sahu_123Bharat_sahu_123Bharat_sahu_123',
   adminSetupToken: 'Bharat_sahu_123Bharat_sahu_123Bharat_sahu_123',
